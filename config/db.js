9fcce142be2695bfc0a77906bdeab2f5a2e13b44
Mohit Kumar <mohitkumar.1828@gmail.com>
2024-11-21 23:40:33 +0530

    Docker changes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/property_management", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
